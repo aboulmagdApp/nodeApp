e80b3b3ce3a32c2b86e28d68c48f31a0af4c5263
Mohamed Aboulmagd <aboulmagd@live.com>
2020-02-05 17:03:19 +0300

    update app.js

--- app.js
+++ app.js
@@ -1,19 +1,25 @@
 const express = require('express');
+//import core module to parse all incoming request
 const bodyParser = require('body-parser');
+// inport path to return application path for static file like css, html...etc
 const path = require('path');
 
 //create application express
 const app = express();
 
+//declare all app routes
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 // use bodyParser for parse incoming request
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')))
+
+// use all routes in app
 app.use('/admin',adminRoutes);
 app.use(shopRoutes);
 
+// handel error page in app
 app.use('/',(req, res, next) =>{
     res.status(404).sendFile(path.join(__dirname,'views','404.html'));
 });
