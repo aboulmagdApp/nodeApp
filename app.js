07be940c8ed33ad23a92bb71104e5a9b331accb1
Mohamed Aboulmagd <aboulmagd@live.com>
2020-02-05 16:13:26 +0300

    serving files statically

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const shopRoutes = require('./routes/shop');
 
 // use bodyParser for parse incoming request
 app.use(bodyParser.urlencoded({extended: false}));
-
+app.use(express.static(path.join(__dirname, 'public')))
 app.use('/admin',adminRoutes);
 app.use(shopRoutes);
 
