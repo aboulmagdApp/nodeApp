848fb5089d2efe13459582049e51bdff86d4885b
Mohamed Aboulmagd <aboulmagd@live.com>
2020-02-05 13:35:55 +0300

    filtering paths

--- app.js
+++ app.js
@@ -9,7 +9,11 @@ const shopRoutes = require('./routes/shop');
 // use bodyParser for parse incoming request
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.use(adminRoutes);
+app.use('/admin',adminRoutes);
 app.use(shopRoutes);
+
+app.use('/',(req, res, next) =>{
+    res.status(404).send('<h1>page not found!</h1>');
+});
 
 app.listen(3000);
