74c4ec9ac791f1e9e281e1381bf325fd3764d613
Mohamed Aboulmagd <aboulmagd@live.com>
2020-02-11 14:35:11 +0300

    adding orders page

--- controllers/shop.js
+++ controllers/shop.js
@@ -33,3 +33,10 @@ exports.getCheckout = (req, res, next) =>{
         pageTitle: 'checkout'
     });
 };
+
+exports.getOrders = (req, res, next) =>{
+    res.render('shop/orders',{
+        path: '/orders',
+        pageTitle: 'Your orders'
+    });
+};
