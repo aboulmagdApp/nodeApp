aa1d99b59ca6e630a8a4bae01af3f29a1deb82d0
Mohamed Aboulmagd <aboulmagd@live.com>
2020-02-12 09:33:38 +0300

    Rendering the Product Detail View

--- models/product.js
+++ models/product.js
@@ -16,14 +16,15 @@ const getProductsFromFile = cb => {
     });
 }
 module.exports = class product {
-    constructor(title, imageUrl, description, price) {
+    constructor(title, imageUrl, price, description) {
         this.title = title;
         this.imageUrl = imageUrl;
-        this.description = description;
         this.price = price;
+        this.description = description;
     }
 
     save() {
+        this.id = Math.random().toString();
         getProductsFromFile(products => {
             products.push(this);
             fs.writeFile(p, JSON.stringify(products), (err) => {
@@ -34,4 +35,11 @@ module.exports = class product {
     static fetchAll(cb) {
         getProductsFromFile(cb);
     };
+
+    static findById(id, cb){
+        getProductsFromFile(products => {
+            const product = products.find(p => p.id === id);
+            cb(product);
+        });
+    }
 }
