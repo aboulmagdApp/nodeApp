7a834eb1a95c571e3c5cfa1ea994ae7184ad2ac8
Mohamed Aboulmagd <aboulmagd@live.com>
2020-02-15 17:45:52 +0300

    adding cart model

--- models/cart.js
+++ models/cart.js
@@ -4,12 +4,36 @@ const rootDir = require('../util/path');
 
 const p = path.join(rootDir,
     'data',
-    'products.json'
+    'cart.json'
 );
 
-module.exports = class cart{
-    constructor(){
-        this.products = [];
-        this.totalPrice = 0;
-    }
+module.exports = class Cart {
+    static addProduct(id, productPrice) {
+        fs.readFile(p, (err, fileContent) => {
+            let cart = { products: [], totalPrice: 0 };
+            if (!err) {
+                cart = JSON.parse(fileContent);
+            }
+            const existingProductIndex = cart.products.findIndex(
+                prod => prod.id === id
+              );
+              const existingProduct = cart.products[existingProductIndex];
+              let updatedProduct;
+            if(existingProduct)
+            {
+                updatedProduct = { ...existingProduct };
+                updatedProduct.qty = updatedProduct.qty + 1;
+                cart.products = [...cart.products];
+                cart.products[existingProductIndex] = updatedProduct;
+            }
+            else {
+                updatedProduct = {id: id, qty: 1};
+                cart.products = [...cart.products, updatedProduct];
+            }
+            cart.totalPrice = cart.totalPrice +  +productPrice;
+            fs.writeFile(p, JSON.stringify(cart), (err) =>{
+                console.log(err);
+            });
+        });
+    };
 }
