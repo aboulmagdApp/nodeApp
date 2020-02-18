d02f8f8453add90e0db02f44274779aa47357f62
Mohamed Aboulmagd <aboulmagd@live.com>
2020-02-19 01:51:44 +0300

    Inserting Data Into the Database

--- models/product.js
+++ models/product.js
@@ -11,18 +11,23 @@ module.exports = class Product {
   }
 
   save() {
-
+     //to create uuid-in-javascript
+     this.id = [...Array(22)].map(i=>(~~(Math.random()*36)).toString(36)).join('');
+     //Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);    
+     // Math.random().toString();
+     return db.execute(
+      'INSERT INTO products (id, title, imageUrl, description, price) VALUES (?, ?, ?, ?, ?)',
+      [this.id, this.title, this.imageUrl, this.description, this.price]
+    );
   }
 
   static deleteById(id) {
-
   }
 
   static fetchAll() {
-   return db.execute('SELECT * FROM products');
+    return db.execute('SELECT * FROM products');
   };
 
   static findById(id) {
-
   }
 }
