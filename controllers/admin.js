da98800bbd377dc5f6936904d8cecd09d7336473
Mohamed Aboulmagd <aboulmagd@live.com>
2020-02-19 14:57:30 +0300

    Fetching Admin Products

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,84 +1,88 @@
 const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
-    res.render('admin/edit-product', {
-        pageTitle: 'add product', 
-        path: '/admin/add-product',
-        editing: false,
-    });
+  res.render('admin/edit-product', {
+    pageTitle: 'add product',
+    path: '/admin/add-product',
+    editing: false,
+  });
 };
 
 exports.getEditProduct = (req, res, next) => {
-    const editMode = req.query.edit;
-    if (!editMode) {
+  const editMode = req.query.edit;
+  if (!editMode) {
+    return res.redirect('/');
+  }
+  const prodId = req.params.productId;
+  Product.findById(prodId, product => {
+    if (!product) {
       return res.redirect('/');
     }
-    const prodId = req.params.productId;
-    Product.findById(prodId, product => {
-      if (!product) {
-        return res.redirect('/');
-      }
-      res.render('admin/edit-product', {
-        pageTitle: 'Edit Product',
-        path: '/admin/edit-product',
-        editing: editMode,
-        product: product
-      });
+    res.render('admin/edit-product', {
+      pageTitle: 'Edit Product',
+      path: '/admin/edit-product',
+      editing: editMode,
+      product: product
     });
-  };
+  });
+};
 
-exports.postEditProduct = (req, res, next) =>{
-    // prouctId we are useing it in edit-product view in hidden input name=prouctId
-    // and every item must mutch name of input in edit-product view
-    const prodId = req.body.productId;
-    const updatedTitle = req.body.title;
-    const updatedPrice = req.body.price;
-    const updatedImageUrl = req.body.imageUrl;
-    const updatedDesc = req.body.description;
-    const updatedProduct = new Product(
-        prodId,
-        updatedTitle,
-        updatedImageUrl,
-        updatedDesc,
-        updatedPrice
-    );
-    updatedProduct.save();
-    res.redirect('/admin/products');
+exports.postEditProduct = (req, res, next) => {
+  // prouctId we are useing it in edit-product view in hidden input name=prouctId
+  // and every item must mutch name of input in edit-product view
+  const prodId = req.body.productId;
+  const updatedTitle = req.body.title;
+  const updatedPrice = req.body.price;
+  const updatedImageUrl = req.body.imageUrl;
+  const updatedDesc = req.body.description;
+  const updatedProduct = new Product(
+    prodId,
+    updatedTitle,
+    updatedImageUrl,
+    updatedDesc,
+    updatedPrice
+  );
+  updatedProduct.save();
+  res.redirect('/admin/products');
 }
 
-exports.postAddProduct =  (req, res, next) => {
-    const id = [...Array(22)].map(i=>(~~(Math.random()*36)).toString(36)).join('');
-    const title = req.body.title;
-    const imageUrl = req.body.imageUrl;
-    const price = req.body.price;
-    const description = req.body.description;
-    Product.create({
-      id,
-      title,
-      price,
-      imageUrl,
-      description
-    })
-    .then(result =>{
+exports.postAddProduct = (req, res, next) => {
+  const id = [...Array(22)].map(i => (~~(Math.random() * 36)).toString(36)).join('');
+  const title = req.body.title;
+  const imageUrl = req.body.imageUrl;
+  const price = req.body.price;
+  const description = req.body.description;
+  Product.create({
+    id,
+    title,
+    price,
+    imageUrl,
+    description
+  })
+    .then(result => {
       console.log('created product');
     })
     .catch(err => {
       console.log(err);
     });
- };
+};
 
- exports.getProducts = (req, res, next) =>{
-    Product.fetchAll(products =>{
-        res.render('admin/products',{
-            prods: products, 
-            pageTitle: 'Admin Products', 
-            path:'/admin/products'
-        });   
+exports.getProducts = (req, res, next) => {
+  Product.findAll()
+    .then(products => {
+      res.render('admin/products', {
+        prods: products,
+        pageTitle: 'Admin Products',
+        path: '/admin/products'
+      });
+    })
+    .catch(err => {
+      console.log(err);
     });
- };
+};
 
- exports.postDeleteProduct = (req, res, next) =>{
+exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
   Product.deleteById(prodId);
   res.redirect('/admin/products');
- }
+}
