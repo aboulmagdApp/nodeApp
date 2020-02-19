5a81a28e6f9f596011dfc41498fa8a4257d942d3
Mohamed Aboulmagd <aboulmagd@live.com>
2020-02-19 14:47:42 +0300

    Getting a Single Product with the "where" Condition using sequelize

--- controllers/shop.js
+++ controllers/shop.js
@@ -17,10 +17,11 @@ exports.getProducts = (req, res, next) => {
 
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
-  Product.findById(prodId)
-    .then(([product]) => {
+  console.log(prodId);
+  Product.findByPk(prodId)
+    .then(product => {
       res.render('shop/product-detail', {
-        product: product[0],
+        product: product,
         pageTitle: product.title,
         path: '/products'
       });
