97471593c2993b9abe52cbcc7fb07b816ad63227
Mohamed Aboulmagd <aboulmagd@live.com>
2020-02-19 11:12:45 +0300

    Defining a Model

--- models/product.js
+++ models/product.js
@@ -1,34 +1,26 @@
-const db = require('../util/database');
-const Cart = require('./cart');
+const Sequelize = require('sequelize');
 
-module.exports = class Product {
-  constructor(id, title, imageUrl, description, price) {
-    this.id = id;
-    this.title = title;
-    this.imageUrl = imageUrl;
-    this.description = description;
-    this.price = price;
+const sequelize = require('../util/database');
+
+const Product = sequelize.define('product', {
+  id:{
+    type: Sequelize.CHAR,
+    allowNull: false,
+    primaryKey: true
+  },
+  title: Sequelize.STRING,
+  price: {
+    type: Sequelize.DOUBLE,
+    allowNull: false
+  },
+  imageUrl:{
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  description:{
+    type: Sequelize.STRING,
+    allowNull: false
   }
+});
 
-  save() {
-     //to create uuid-in-javascript
-     this.id = [...Array(22)].map(i=>(~~(Math.random()*36)).toString(36)).join('');
-     //Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);    
-     // Math.random().toString();
-     return db.execute(
-      'INSERT INTO products (id, title, imageUrl, description, price) VALUES (?, ?, ?, ?, ?)',
-      [this.id, this.title, this.imageUrl, this.description, this.price]
-    );
-  }
-
-  static deleteById(id) {
-  }
-
-  static fetchAll() {
-    return db.execute('SELECT * FROM products');
-  };
-
-  static findById(id) {
-    return db.execute('SELECT * FROM products WHERE products.id = ?', [id]);
-  }
-}
+module.exports = Product;
