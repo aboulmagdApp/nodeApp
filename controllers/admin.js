67d90c9765ac7baef17a0cda5220b39197ffcac0
Mohamed Aboulmagd <aboulmagd@live.com>
2020-02-19 12:29:06 +0300

    inserting data using sequelize

--- controllers/admin.js
+++ controllers/admin.js
@@ -47,17 +47,24 @@ exports.postEditProduct = (req, res, next) =>{
 }
 
 exports.postAddProduct =  (req, res, next) => {
-      // and every item must mutch name of input in edit-product view
+    const id = [...Array(22)].map(i=>(~~(Math.random()*36)).toString(36)).join('');
     const title = req.body.title;
     const imageUrl = req.body.imageUrl;
     const price = req.body.price;
     const description = req.body.description;
-    const product = new Product(null, title, imageUrl, description, price);
-    product.save()
-    .then(() =>{
-      res.redirect('/');
+    Product.create({
+      id,
+      title,
+      price,
+      imageUrl,
+      description
     })
-    .catch(err => console.log(err));
+    .then(result =>{
+      console.log('created product');
+    })
+    .catch(err => {
+      console.log(err);
+    });
  };
 
  exports.getProducts = (req, res, next) =>{
