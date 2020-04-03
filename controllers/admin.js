9c3be5673502da60bef0ad3563f344ba71dbeb9f
Mohamed Aboulmagd <aboulmagd@live.com>
2020-04-04 02:34:13 +0300

    Updating Products

--- controllers/admin.js
+++ controllers/admin.js
@@ -42,9 +42,14 @@ exports.postEditProduct = (req, res, next) => {
   const updatedImageUrl = req.body.imageUrl;
   const updatedDesc = req.body.description;
 
-  const product = new Product(updatedTitle, updatedImageUrl, updatedDesc, updatedPrice, new ObjectId(prodId));
-  product
-    .save()
+ //const product = new Product(updatedTitle, updatedImageUrl, updatedDesc, updatedPrice, new ObjectId(prodId));
+  Product.findById(prodId).then(product =>{
+    product.title = updatedTitle;
+    product.price = updatedPrice;
+    product.description = updatedDesc;
+    product.imageUrl = updatedImageUrl;
+    return product.save();
+  })
     .then(result => {
       console.log('UPDATED PRODUCT!');
       res.redirect('/admin/products');
@@ -76,7 +81,7 @@ exports.postAddProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll()
+  Product.find()
     .then(products => {
       res.render('admin/products', {
         prods: products,
