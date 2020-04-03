aea958cbde5f475ea200af590477fb86c88a51c3
Mohamed Aboulmagd <aboulmagd@live.com>
2020-04-04 02:07:39 +0300

    Saving Data Through Mongoose

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const express = require('express');
 //import core module to parse all incoming request
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 // inport path to return application path for static file like css, html...etc
 const path = require('path');
 
 // working with login in error controller
 const errorController = require('./controllers/error');
-const mongoConnect = require('./util/database').mongoConnect;
-const User = require('./models/user');
+// const User = require('./models/user');
 
 //create application express
 const app = express();
@@ -26,14 +26,14 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')))
 
 // this middleware will run every request
-app.use((req, res, next) => {
-    User.findById("5e67beacf53b714b544fbdab")
-        .then(user => {
-            req.user = new User(user.name, user.email, user.cart, user._id) ;
-            next();
-        })
-        .catch(err => console.log(err));
-});
+// app.use((req, res, next) => {
+//     User.findById("5e67beacf53b714b544fbdab")
+//         .then(user => {
+//             req.user = new User(user.name, user.email, user.cart, user._id) ;
+//             next();
+//         })
+//         .catch(err => console.log(err));
+// });
 // use all routes in app
  app.use('/admin', adminRoutes);
  app.use(shopRoutes);
@@ -41,7 +41,11 @@ app.use((req, res, next) => {
 // handel error page in app
 app.use(errorController.get404);
 
-mongoConnect(() => {
-
-    app.listen(3000);
-});
+mongoose
+    .connect('mongodb://aboulmagd:aqtkOcbhYbQD0biM@cluster0-shard-00-00-vjxvu.mongodb.net:27017,cluster0-shard-00-01-vjxvu.mongodb.net:27017,cluster0-shard-00-02-vjxvu.mongodb.net:27017/shop?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority'
+    ).then(result =>{
+        app.listen(3000);
+    })
+    .catch(err =>{
+        console.log(err);
+    })
--- routes/admin.js
+++ routes/admin.js
@@ -6,20 +6,20 @@ const adminController = require('../controllers/admin');
 
 const router = express.Router();
 
-// /admin/add-product  => GET
-router.get('/add-product', adminController.getAddProduct);
+// // /admin/add-product  => GET
+ router.get('/add-product', adminController.getAddProduct);
 
-// /admin/products  => GET
-router.get('/products', adminController.getProducts);
+// // /admin/products  => GET
+// router.get('/products', adminController.getProducts);
 
-// /admin/add-product  => POST
-router.post('/add-product', adminController.postAddProduct);
+// // /admin/add-product  => POST
+ router.post('/add-product', adminController.postAddProduct);
 
-router.get('/Edit-product/:productId', adminController.getEditProduct);
+// router.get('/Edit-product/:productId', adminController.getEditProduct);
 
-router.post('/Edit-product', adminController.postEditProduct);
+// router.post('/Edit-product', adminController.postEditProduct);
 
-router.post('/delete-product', adminController.postDeleteProduct);
+// router.post('/delete-product', adminController.postDeleteProduct);
 
 module.exports = router;
 // exports.routes = router;
