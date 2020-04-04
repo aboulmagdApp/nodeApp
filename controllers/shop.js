2c8dfa27093cc453cf23246268ba93fc9149e271
Mohamed Aboulmagd <aboulmagd@live.com>
2020-04-05 02:33:38 +0300

    Storing All Order Related Data

--- controllers/shop.js
+++ controllers/shop.js
@@ -93,8 +93,9 @@ exports.postOrder = (req, res, next) => {
     .populate('cart.items.productId')
     .execPopulate()
     .then(user => {
+      console.log(user.cart.items);
       const products = user.cart.items.map(i =>{
-        return {quantity: i.quantity, product: i.productId}
+        return {quantity: i.quantity, product: { ...i.productId._doc}} // ._doc spetial field in mongo db can attach all data in object
       });
       const order = new Order({
         user: {
