8f34b1118f585b0a94d8118db58f3c4cbc94f0c9
Mohamed Aboulmagd <aboulmagd@live.com>
2020-04-04 03:29:56 +0300

    Using Relations in Mongoose

--- controllers/admin.js
+++ controllers/admin.js
@@ -68,7 +68,9 @@ exports.postAddProduct = (req, res, next) => {
     title: title, 
     price: price, 
     description: description, 
-    imageUrl: imageUrl});
+    imageUrl: imageUrl,
+    userId: req.user._id
+  });
   product
     .save()
     .then(result => {
