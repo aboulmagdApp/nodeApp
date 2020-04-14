2cd50e5773de59e5d10a0ef26feb9d0de722e8d5
Mohamed Aboulmagd <aboulmagd@live.com>
2020-04-14 21:50:33 +0300

    Implementing an Authentication Flow

--- controllers/auth.js
+++ controllers/auth.js
@@ -8,6 +8,14 @@ exports.getLogin = (req, res, next) => {
     });
 };
 
+exports.getSignup = (req, res, next) => {
+    res.render('auth/signup', {
+      path: '/signup',
+      pageTitle: 'Signup',
+      isAuthenticated: false
+    });
+  };
+
 exports.postLogin = (req, res, next) => {
     User.findById('5e87d25a0c7a0a2404f4f42e')
     .then(user =>{
@@ -17,6 +25,28 @@ exports.postLogin = (req, res, next) => {
             console.log(err);
             res.redirect('/')
         });
+    })
+    .catch(err => console.log(err));
+};
+
+exports.postSignup = (req, res, next) => {
+    const email = req.body.email;
+    const password = req.body.password;
+    const confirmPassword = req.body.confirmPassword;
+    User.findOne({email: email})
+    .then(userDoc => {
+        if(userDoc){
+           return res.redirect('/signup') 
+        }
+        const user = new User({
+            email: email,
+            password: password,
+            cart: {items: []}
+        })
+        return user.save();
+    })
+    .then(result =>{
+        res.redirect('/login');    
     })
     .catch(err => console.log(err));
 };
